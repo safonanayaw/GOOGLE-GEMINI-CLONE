c79e020ee9356ea14fd3631a9b5e00a39d44fbd6
Nana <healien@Nanas-MacBook-Pro.local>
2024-08-15 05:28:06 +0100

    menu icon toggle to colapse when click and open when click

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,36 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Sidebar.css'
 import {assets} from '../../assets/assets'
 
+
 function Sidebar() {
+
+    const [extended, setExtended] = useState(false);
+
   return (
     <div className='sidebar'>
         <div className="top">
-            <img className='menu' src={assets.menu_icon} alt="" />
+            {/* onClick function on menu img to set setExtended previous value to opposite the prev value true to false and vice versa*/}
+            <img onClick={()=>{setExtended(prev => !prev)}} 
+            className='menu' src={assets.menu_icon} alt="" />
+
             <div className="new-chat">
                 <img src={assets.plus_icon} alt="" />
-                <p>New Chat</p>
+                {/* if menu icon is false with ternary operator display none */}
+                {extended ? <p>New Chat</p> : null}
             </div>
-            <div className="recent">
-                <p className="recent-title">Recent</p>
-                <div className="recent-entry">
-                    <img src={assets.message_icon} alt="" />
-                    <p>What is react ...</p>
-                </div>
-            </div>
+
+                {/*start if menu icon is false with ternary operator display none */}
+
+            {extended ? 
+                        <div className="recent">
+                        <p className="recent-title">Recent</p>
+                        <div className="recent-entry">
+                            <img src={assets.message_icon} alt="" />
+                            <p>What is react ...</p>
+                        </div>
+                    </div> : null
+        }
+                {/*end if menu icon is false with ternary operator display none */}
+
         </div>
         <div className="bottom">
             <div className="bottom-item recent-entry">
                 <img src={assets.question_icon} alt="" />
-                <p>Help</p>
+
+                {/* if menu icon is false with ternary operator display none */}
+                {extended ? <p>Help</p> : null}
             </div>
             <div className="bottom-item recent-entry">
                 <img src={assets.history_icon} alt="" />
-                <p>Activity</p>
+
+                {/* if menu icon is false with ternary operator display none */}
+                {extended ? <p>Activity</p> : null}
+
             </div>
             <div className="bottom-item recent-entry">
                 <img src={assets.setting_icon} alt="" />
-                <p>Settings</p>
+
+                {/* if menu icon is false with ternary operator display none */}
+                {extended ? <p>Settings</p> : null}
             </div>                        
         </div>
     </div>
